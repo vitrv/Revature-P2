5b7946a0f34bc6ec4208820f21964dac15128bdb
‘Immanuel <immanuelvatta@gmail.com>
2019-09-09 11:29:13 -0500

    changed database from local to azure

--- Acedrive.Data/AcedriveDbContext.cs
+++ Acedrive.Data/AcedriveDbContext.cs
@@ -16,9 +16,9 @@ namespace Acedrive.Data
       if (!builder.IsConfigured)
       {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-        builder.UseSqlServer("server=localhost;initial catalog=AceDriveDb;user id =sa; password=Password12345;");
+        //builder.UseSqlServer("server=localhost;initial catalog=AceDriveDb;user id =sa; password=Password12345;");
 
-        //builder.UseSqlServer("Server=tcp:lnc-sql.database.windows.net,1433;Initial Catalog=DriveDB;Persist Security Info=False;User ID=sqladmin;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        builder.UseSqlServer("Server=tcp:lnc-sql.database.windows.net,1433;Initial Catalog=DriveDB;Persist Security Info=False;User ID=sqladmin;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
       }
     }
     protected override void OnModelCreating(ModelBuilder builder)
