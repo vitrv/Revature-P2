bad526a140c1be51a79462f8a45b86ac0118cdc0
Laura Catano <laura.n.catano@gmail.com>
2019-09-11 21:32:43 -0500

    added session handler example to home controller

--- Acedrive.Client/Controllers/HomeController.cs
+++ Acedrive.Client/Controllers/HomeController.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Acedrive.Client.Models;
 using Microsoft.Extensions.Options;
+using Acedrive.Domain.Models;
 
 namespace Acedrive.Client.Controllers
 {
-    
+
     public class HomeController : Controller
     {
+        Session _session = SessionHandler.Instance();
         public IActionResult Index()
         {
+          //retrieve list of all cars in db with
+          List<Vehicle> vehicles = _session.GetAllVehicles();
             return View();
         }
 
