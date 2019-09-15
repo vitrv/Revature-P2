9b0021bbe1f3653604fa338ae14c2c34e5d4656e
Laura Catano <laura.n.catano@gmail.com>
2019-09-15 18:33:35 -0500

    azure secrets attempt #2

--- Acedrive.Client/Startup.cs
+++ Acedrive.Client/Startup.cs
@@ -50,7 +50,7 @@ namespace Acedrive.Client
             }
             else
             {
-                _connectionString = Configuration.GetConnectionString("ConnectionString:Secret");
+                _connectionString = Configuration.GetConnectionString("SQLCONNSTR_Secret");
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
